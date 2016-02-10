51272621dbe80416626728d3f0eabf7ab5052e1a
aquaye <fusein.myhead@gmail.com>
2016-02-09 22:09:42 -0500

    Homework
    Completed assignment, added and passed tests

--- lesson_01_homework.rb
+++ lesson_01_homework.rb
@@ -26,13 +26,40 @@ end
 
 ## Enter your code for your 'add' method below
 
+def add(number_1, number_2)
+  return number_1 + number_2
+end
 
+puts "Three tests"
 
+puts add(1,2)
+puts add(5,10)
+puts add(4,4)
 
 ## Enter the code for you 'join_strings' method below
 
+def join_strings(string_1, string_2)
+  return string_1.to_s + " " + string_2.to_s
+end
 
+puts "Four tests"
 
-
+puts join_strings("deez", "nuts")
+puts join_strings(1,2)
+puts join_strings(false, "false")
+puts join_strings([1,"2",true], 2.5)
 
 ## Enter your 'old_enough_to_vote?' method below
+
+def old_enough_to_vote(age)
+  if age >= 18
+    return "You are old enough to vote!"
+  else
+    return "You are not old enough to vote!"
+  end
+end
+
+puts "Two tests"
+
+puts old_enough_to_vote(17)
+puts old_enough_to_vote(19)
